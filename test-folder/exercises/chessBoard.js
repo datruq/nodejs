4b948d82695cba79d14b67474b98d84d9986d059
datruq <dtrujo@outlook.com>
2020-08-20 21:43:42 -0500

    O(n) implementations added

--- test-folder/exercises/chessBoard.js
+++ test-folder/exercises/chessBoard.js
@@ -1,5 +1,6 @@
-chessBoard(40, 40);
-
+/*
+* brute force
+*/
 function chessBoard(w = 4, h = 4) {
     var odd = true;
     var board = '';
@@ -13,4 +14,35 @@ function chessBoard(w = 4, h = 4) {
         console.log(board);
         board = '';
     }
+}
+chessBoard(6,6);
+console.log('---------best answer----------')
+chessBoardBest(6,6);
+
+/*
+* O(n)
+*/
+function chessBoardBest(w = 4, h = 4) {
+    var board = [];
+    var pairRow = [];
+    var oddRow = [];
+    var max = w > h ? w : h;
+    for (let i = 0; i <= max; i++) {        
+        if (isOdd(i)) board.push(true);
+        else board.push(false);        
+    }
+
+    for (const n of board) {
+        if(n) oddRow.push('# ');
+        else pairRow.push(' #');
+    }
+
+    for (const n of board) {     
+        if (n)  console.log(oddRow.join(''));
+        else console.log(pairRow.join(''));      
+    }
+}
+
+function isOdd(num) {
+    return num % 2 == 0 ? true : false;
 }
--- test-folder/exercises/hacker-rank.js
+++ test-folder/exercises/hacker-rank.js
@@ -1,17 +1,45 @@
-sumDiagonals([[11,2,4],[4,5,6],[10,8,-12]]);
+sumDiagonals([
+  [11, 2, 4],
+  [4, 5, 6],
+  [10, 8, -12],
+]);
 
-function sumDiagonals(arr){
-    var sumDiag = 0;
-    var sumSecDiag = 0;
-    for (let i = 0; i < arr.length; i++) {
-        for (let j =0; j < arr.length; j++){
-            if (i===j) {
-                sumDiag += arr[i][j];
-            }
-            if (i + j === arr.length-1) {
-                sumSecDiag += arr[i][j];
-            }
-        }
+sumDiagonalsBest([
+  [11, 2, 4],
+  [4, 5, 6],
+  [10, 8, -12],
+]);
+
+function sumDiagonals(arr) {
+  var sumDiag = 0;
+  var sumSecDiag = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (i === j) {
+        sumDiag += arr[i][j];
+      }
+      if (i + j === arr.length - 1) {
+        sumSecDiag += arr[i][j];
+      }
     }
-    console.log('la suma es', Math.abs(sumSecDiag-sumDiag));
+  }
+  console.log("la suma es", Math.abs(sumSecDiag - sumDiag));
+}
+
+/*
+ * best answer
+ */
+function sumDiagonalsBest(arr) {
+  var sumDiag = 0;
+  var sumSecDiag = 0;
+  for (let i = 0; i < arr.length; i++) {
+    var secundaryPosition = getsecundaryDiagonalPosition(arr, i);
+    sumDiag += arr[i][i];
+    sumSecDiag += arr[i][secundaryPosition];
+  }
+  console.log("best la suma es", Math.abs(sumSecDiag - sumDiag));
+}
+
+function getsecundaryDiagonalPosition(arr, num) {
+  return (arr.length - 1) - num;
 }
