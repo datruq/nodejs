cc14e09330c9bf5b5900d03435241f12fd0d789e
David Trujillo <david.trujillo@LEON-MC-200818B.local>
2020-08-26 00:54:53 -0500

    O(n) solution added

--- test-folder/exercises/findIntersection.js
+++ test-folder/exercises/findIntersection.js
@@ -13,6 +13,25 @@ function FindIntersection(strArr) {
     return res.length === 0 ? false : res.join(',');
   }
 
-  console.log(FindIntersection(['1, 3, 5, 7, 9', '0 , 8, 9']))
+  function FindIntersectionBest(strArr) {
+    var res = [];
+    var map = {}
+    var fisrtArr = strArr[0].split(', ');
+    var secondArr = strArr[1].split(', ');
+    for(let f of fisrtArr){
+        map[f] = (map[f] || 0 ) + 1;
+    }
+    for (let s of secondArr) {
+        map[s] = (map[s] || 0 ) + 1;
+    }
+    for (let m in map) {
+        if(map[m] > 1){
+            res.push(m);
+        }
+    }
+    return res.length === 0 ? false : res.join(',');
+  }
 
-  
+  console.log(FindIntersection(['1, 3, 5, 7, 9', '0 , 8, 9']))
+  console.log(FindIntersectionBest(['1, 3, 5, 7, 9', '0 , 8, 9']))
+
