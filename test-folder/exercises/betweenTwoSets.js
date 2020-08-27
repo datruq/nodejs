2edd192f7bb3d781c03ce2182841037c5d597667
datruq <dtrujo@outlook.com>
2020-08-26 21:46:38 -0500

    betweenTwoSets complete answer

--- test-folder/exercises/betweenTwoSets.js
+++ test-folder/exercises/betweenTwoSets.js
@@ -2,20 +2,41 @@ function getTotalX(a, b) {
     var start = a[a.length - 1];
     var end = b[0];
     var res = [];
-    debugger;
     for (let i = start; i < end; i++) {
         var aPass = true, bPass = true;
-        for (let s of a) {    
+        for (let s of a) {
             if (i % s !== 0) aPass = false;
         }
 
         if (!aPass) continue;
-        for (let x of b) {    
+        for (let x of b) {
             if (x % i !== 0) bPass = false;
         }
         if (aPass && bPass) res.push(i);
     }
-    console.log(res);
+    console.log(res.length);
 }
 
-getTotalX([2,6], [24, 36]);
+getTotalX([2, 6], [24, 36]);
+getTotalXBest([2, 4], [16, 32, 96]);
+
+function getTotalXBest(a, b) {
+    var lastA = a[a.length - 1];
+    var firstB = b[0];
+    var mapA = {};
+    var mapB = {};
+    var res = [];
+    for (let i = lastA; i <= firstB; ++i) {
+        for (let ofA of a) {
+            if (i % ofA === 0) mapA[i] = (mapA[i] || 0) + 1;
+        }
+        for (let ofB of b) {
+            if (ofB % i === 0) mapB[i] = (mapB[i] || 0) + 1;
+        }
+    }
+    for (let i in mapA) {
+        if (mapA[i] === a.length && mapB[i] === b.length)
+            res.push(i);
+    }
+    console.log(res.length);
+}
---
+++ test-folder/exercises/bracketMatcher.js
@@ -0,0 +1,20 @@
+function BracketMatcher(str) {
+    var reg = [];
+    var count = 0;
+    if (!/[()]/.test(str)) return 1;
+    for (let par of str) {
+        if (/[()]/.test(par))
+            reg.push(par);
+    }
+    debugger;
+    for (let par of reg) {
+        if (par === '(') ++count;
+        if (par === ')') --count;
+        if (count < 0) return 0;
+    }
+    return count === 0 ? 1 : 0;
+
+}
+
+// keep this function call here 
+console.log(BracketMatcher('(coder)(byte))'));
